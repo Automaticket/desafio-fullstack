6c6e968dfa1e44f85cd61a5511d557ce776beabb
Genivaldo Silva <genivaldosilva@gmail.com>
2019-07-24 10:47:30 -0300

    Initial setup
    Install and config passport;
    Move models to folder models.

--- api/database/factories/UserFactory.php
+++ api/database/factories/UserFactory.php
@@ -1,9 +1,9 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
-use Illuminate\Support\Str;
+use App\Models\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
