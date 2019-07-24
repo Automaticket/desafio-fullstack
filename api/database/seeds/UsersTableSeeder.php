6ed7da741f774154b4a003a9e6d2dd0194d84ccc
Genivaldo Silva <genivaldosilva@gmail.com>
2019-07-24 10:56:41 -0300

    Config initial
    Created default admin user.

---
+++ api/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        factory(User::class)->create([
+            'email' => 'admin@email.com'
+        ]);
+    }
+}
